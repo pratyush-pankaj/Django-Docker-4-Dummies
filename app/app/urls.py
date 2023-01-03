9d913162b755e3f6b1c6c5fa4b055a59aeb2524f
Pratyush Pankaj <pratyushpankaj@Pratyushs-MacBook-Pro.local>
2023-01-04 04:37:15 +0530

    Remove dental app for generic template

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,5 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('dental/', include('dental.urls'))
+    # path('dental/', include('dental.urls'))
 ]
